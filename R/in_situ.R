0980e5d8df9ad8a0bf215562490a137d556c1403
YoungjunNa <ruminoreticulum@gmail.com>
2017-11-10 18:31:08 +0900

    fix some error

--- R/in_situ.R
+++ R/in_situ.R
@@ -6,8 +6,8 @@
 #Date: 9/25/2017
 
 #Degradation parameter was made using the formula of Ørskov and McDonald (1979): P = a + b (1 - e^(-ct))
-#where P: the actual degradation after time ‘t’; 
-#a: the intercept of the degradation curve at time zero; 
+#where P: the actual degradation after time ‘t’;
+#a: the intercept of the degradation curve at time zero;
 #b: the potential degradability of the component of the protein which will, in time, be degraded; c: the rate constant for the degradation of ‘b’;
 #t: time.
 
@@ -25,37 +25,32 @@
 #Littell, R. C., P. R. Henry, and C. B. Ammerman. 1998. Statistical analysis of repeated measures data using SAS procedures. Journal of animal science. 76:1216-1231.
 #Ørskov, E. R., and I. McDonald. 1979. The estimation of protein degradability in the rumen from incubation measurements weighted according to rate of passage. J. Agric. Sci. 92:499.
 
-
-hour<-c(0,2,4,8,16,24,48)
-DMD<-c(90,88,80,75,58,43,10)
-
 insitu<-function(hour,DMD){
-  
+
   require(dplyr)
 
   df<-data.frame(hour,DMD)
   n<-nrow(df)
-  
+
   df1<-mutate(df, DMD_1=(DMD-DMD[n])/100)
   df2<-df1[-n,]
   df2<-mutate(df2, DMD_exp=(log(DMD_1,exp(1))))
-  
+
   reg<-lm(df2$DMD_exp~df2$hour)
   reg<-reg$coefficients
   intercept<-reg[1]
   x<-reg[2]
-  
+
   b<-exp(intercept)*100
   a<-100-b-df1$DMD[n]
   c<-(-x)
   ED2<-a+((b*c)/(c+0.02))
   ED5<-a+((b*c)/(c+0.05))
   ED8<-a+((b*c)/(c+0.08))
-  
+
   result<-data.frame(Item=c("a","b","c","ED2","ED5","ED8"), Value=c(a,b,c,ED2,ED5,ED8))
-  
-  print(result) 
-  
+
+  print(result)
+
 }
 
-insitu(hour,DMD)
